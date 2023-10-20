ca9955acd408a8a2c4515274ed067fabaaa61eef
Luis Donis <ldonis.emc2@gmail.com>
2023-10-19 19:13:32 -0600

    feat: add correct nameclasses

--- app/models/lesli_audit/account.rb
+++ app/models/lesli_audit/account.rb
@@ -1,6 +1,6 @@
 module LesliAudit
     class Account < ApplicationRecord
-        belongs_to :account, :class_name => "Lesli::Account"
+        belongs_to :account, class_name: "Lesli::Account"
         has_many :account_requests
         has_many :user_requests
     end
