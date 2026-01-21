d754ffa4eb4642628643cdd663b789e0fa161713
Luis Donis <ldonis.emc2@gmail.com>
2026-01-20 20:36:20 -0600

    session id must be optional

--- app/models/lesli_audit/user_log.rb
+++ app/models/lesli_audit/user_log.rb
@@ -2,7 +2,7 @@ module LesliAudit
     class UserLog < ApplicationRecord
         belongs_to :account
         belongs_to :user, class_name: "Lesli::User"
-        belongs_to :session, class_name: "LesliShield::User::Session"
+        belongs_to :session, class_name: "LesliShield::User::Session", optional: true
         
         enum :operation, { 
             user_creation: 'user_creation',
